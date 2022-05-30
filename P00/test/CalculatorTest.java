9406987beca2d395878d9119e63dde40206f3fd1
Zongngee <Zongngee42@gmail.com>
2022-05-30 23:14:51 +0800

    first push to P00 repository in GitHUb

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -24,48 +24,67 @@ public class CalculatorTest {
 	public void tearDown() throws Exception {
 	}
 
+	int min = 0;
+	int max = 9999;
+	
 	@Test
 	public void testAdd() {
 		int a = 1234;
 		int b = 8765;
 		
-		Calculator cal = new Calculator();
-		int actual = cal.add(a, b);
-		int expected = 9999;
-		assertEquals(expected, actual);
+		if (max > b && b > min && max > a && a > min) {
+			Calculator cal = new Calculator();
+			int actual = cal.add(a, b);
+			int expected = 9999;
+			assertEquals(expected, actual);
+		} else {
+			fail("Error, number not in the range");
+		}
+		
 	}
 	@Test
 	public void testSubtract() {
 		int b = 4321;
 		int a = 9876;
 		
-		Calculator cal = new Calculator();
-		int actual = cal.subtract(a, b);
-		
-		int expected = 5555;
-		assertEquals(expected, actual);
+		if (max > b && b > min && max > a && a > min) {
+			Calculator cal = new Calculator();
+			int actual = cal.subtract(a, b);
+			int expected = 5555;
+			assertEquals(expected, actual);
+		} else { 
+			fail("Error, number not in the range");
+
+		}
 	}
 	@Test
 	public void testMultiple() {
 		int a = 1234;
 		int b = 8765;
 		
-		Calculator cal = new Calculator();
-		int actual = cal.multiple(a, b);
-		
-		int expected = 10816010;
-		assertEquals(expected, actual);
+		if (max > b && b > min && max > a && a > min) {
+			Calculator cal = new Calculator();
+			int actual = cal.multiple(a, b);
+			int expected = 10816010;
+			assertEquals(expected, actual);
+		} else { 
+			fail("Error, number not in the range");
+
+		}
 	}
 	@Test
 	public void testDivide() {
 		int a = 5555;
 		int b = 1111;
 		
-		Calculator cal = new Calculator();
-		int actual = cal.divide(a, b);
-		
-		int expected = 5;
-		assertEquals(expected, actual);
+		if (max > b && b > min && max > a && a > min) {
+			Calculator cal = new Calculator();
+			int actual = cal.divide(a, b);
+			int expected = 5;
+			assertEquals(expected, actual);
+		} else { 
+			fail("Error, number not in the range");
 	}
 	
+	}
 }
